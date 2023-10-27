dc7dc5335bb5834e0a385a5624362d0187714e1c
Ahmed-Taimoor <malikahmadtamoor@gmail.com>
2023-10-28 03:53:28 +0500

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4ab40178-d011-4fe9-89b3-dd0e872b1d46';
+export const MODAL_CLOSE_SEC = 2.5;
